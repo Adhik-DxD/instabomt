a2ece3751325b090fd248e19fc280cdf326f7368
Adhik-DxD <69236738+Adhik-DxD@users.noreply.github.com>
2023-09-02 23:00:45 +0530

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -5,7 +5,7 @@ import re
 import time
 from instapy_cli import client
 
-def download_and_post_to_instagram(entry, caption):
+def download_and_post_to_instagram(entry, username, password):
     try:
         url = entry.media_content[0]['url']
         img_filename = "imgmain.jpg"
@@ -30,8 +30,20 @@ if __name__ == "__main__":
 
     try:
         print("Logging in to Instagram...")
-        with client(username, password) as api:
-            api.login()
+        # Modify the logic to loop through entries and call download_and_post_to_instagram
+        for entry in reversed(NewsFeed.entries):
+            try:
+                entry_id = entry.get('id')
+                if entry_id == latest_entry_id:
+                    print("All new posts have been processed.")
+                    break
+                print("New feed item found:")
+                print(clean_html_tags(entry.description))
+                caption = entry.title + '\n' + clean_html_tags(entry.description) + '\n\n' + tags
+                download_and_post_to_instagram(entry, username, password)
+                latest_entry_id = entry_id
+            except Exception as e:
+                print(f"An error occurred: {e}")
         print("Logged in to Instagram successfully.")
     except Exception as e:
         print(f"Error logging into Instagram: {e}")
@@ -53,7 +65,7 @@ if __name__ == "__main__":
                 print("New feed item found:")
                 print(clean_html_tags(entry.description))
                 caption = entry.title + '\n' + clean_html_tags(entry.description) + '\n\n' + tags
-                download_and_post_to_instagram(entry, caption)
+                download_and_post_to_instagram(entry, username, password)
                 latest_entry_id = entry_id
             except Exception as e:
                 print(f"An error occurred: {e}")
@@ -62,3 +74,4 @@ if __name__ == "__main__":
         time.sleep(600)  # Sleep for 10 minutes before checking again
 
     print("Script execution completed.")
+    
